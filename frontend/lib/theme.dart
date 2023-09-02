185f0bd48e3461e84057ea943de2cb96b97566c6
junyangGitHub <yang242@hotmail.com>
2023-09-02 20:31:13 +0800

    Revert "Revert "DIP-25""
    This reverts commit 2bed3bcb86c7edfaf06eaaa5836c3a8d29221b60.

---
+++ frontend/lib/theme.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/cupertino.dart';
+
+class CustomTheme {
+  const CustomTheme();
+
+  static const Color loginGradientStart = Color(0xFF5C795B); // add "FF" in front for full oppacity
+  static const Color loginGradientEnd = Color(0xFF5D7395);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+
+  static const LinearGradient primaryGradient = LinearGradient(
+    colors: <Color>[loginGradientStart, loginGradientEnd],
+    stops: <double>[0.0, 1.0],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+}
