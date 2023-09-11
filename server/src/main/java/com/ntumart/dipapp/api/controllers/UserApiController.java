ed08cbe45c551e0793df53077e5ebd404707672f
junyangGitHub <yang242@hotmail.com>
2023-09-11 17:24:04 +0800

    [Fix] CORS error
    allow all origin

--- server/src/main/java/com/ntumart/dipapp/api/controllers/UserApiController.java
+++ server/src/main/java/com/ntumart/dipapp/api/controllers/UserApiController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:*")
+@CrossOrigin(origins = "*")
 public class UserApiController {
     @Autowired
     UserService userService;
