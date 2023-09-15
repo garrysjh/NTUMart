b9c4ff236dac47611a0665009ec5e26b59d3c8dc
junyangGitHub <yang242@hotmail.com>
2023-09-16 01:55:30 +0800

    Uploading Profile Image and Creating File Directory for Images

---
+++ server/src/main/java/com/ntumart/dipapp/api/controllers/ProfileUploadController.java
@@ -0,0 +1,40 @@
+package com.ntumart.dipapp.api.controllers;
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ntumart.dipapp.api.service.ProfUploadService;
+
+import net.minidev.json.JSONObject;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProfileUploadController {
+
+    @Autowired
+    ProfUploadService profUploadService;
+
+    @PostMapping("/{userId}/update-profile-pic")
+    public ResponseEntity<String> uploadImage(@PathVariable Integer userId, @RequestParam("profilePic") MultipartFile file) {
+
+
+        try {
+            
+            profUploadService.uploadFile(userId,file);
+
+            return ResponseEntity.ok("Image uploaded successfully!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image.");
+            
+        }
+    }
+}
+
--- server/src/main/java/com/ntumart/dipapp/api/service/UserService.java
+++ server/src/main/java/com/ntumart/dipapp/api/service/UserService.java
@@ -2,6 +2,9 @@ package com.ntumart.dipapp.api.service;
 
 import com.ntumart.dipapp.api.repository.UserRepository;
 import com.ntumart.dipapp.models.User;
+
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
@@ -15,7 +18,24 @@ public class UserService {
 //        return userRepository.registerUser(username, fullname, email, phone, password, address, profilePic);
 //    }
 public void registerUser(User user){
+
     userRepository.save(user);
+
+    // Gets current UserID and Creates Image Directories
+    Integer userId = user.getUserId();
+
+    String userFolderPath = "images\\" + userId + "\\profpic";
+    File userFolder = new File(userFolderPath);
+    
+    if (userFolder.mkdirs()) {
+        // Creates File Directory returns true
+        System.out.println("Directory is created");
+    }
+    else {
+        // No Directory is created returns false
+        System.out.println("Directory cannot be created");
+    }
+
 }
 
     public int checkExistingUsername(String username){return userRepository.checkExistingUsername(username); }
