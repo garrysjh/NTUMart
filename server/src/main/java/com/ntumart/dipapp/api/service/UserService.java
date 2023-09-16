c95400d4eebcfaa6e1c3bd8e9fdc1d003459dbda
yongjin372k <leeyongjin372k@gmail.com>
2023-09-16 20:45:39 +0800

    Update UserService.java

--- server/src/main/java/com/ntumart/dipapp/api/service/UserService.java
+++ server/src/main/java/com/ntumart/dipapp/api/service/UserService.java
@@ -2,6 +2,9 @@ package com.ntumart.dipapp.api.service;
 
 import com.ntumart.dipapp.api.repository.UserRepository;
 import com.ntumart.dipapp.models.User;
+
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,36 @@ public class UserService {
 
 public void registerUser(User user){
     userRepository.save(user);
+
+    // Gets current UserID and Creates Image Directories
+    Integer userId = user.getUserId();
+
+    String userFolderPath = "images\\" + userId + "\\profpic";
+    File userFolder = new File(userFolderPath);
+    
+    if (userFolder.mkdirs()) {
+        // Creates File Directory returns true
+        System.out.println("Directory is created");
+    }
+    else {
+        // No Directory is created returns false
+        System.out.println("Directory cannot be created");
+    }
+
+
+    // Create Directory for Users' Products
+    String prodFolderPath = "images\\" + userId + "\\products";
+    File prodImgFolder = new File(prodFolderPath);
+
+    if (prodImgFolder.mkdirs()) {
+        // Creates File Directory returns true
+        System.out.println("Directory is created");
+    }
+    else {
+        // No Directory is created returns false
+        System.out.println("Directory cannot be created");
+    }
+
 }
 
     public int checkExistingUsername(String username){return userRepository.checkExistingUsername(username); }
