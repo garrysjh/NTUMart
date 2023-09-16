04f2697eca7079eeee83419d6f34c23ce6656308
junyangGitHub <yang242@hotmail.com>
2023-09-16 20:05:04 +0800

    [Fix] Styling

--- frontend/lib/item_card.dart
+++ frontend/lib/item_card.dart
@@ -11,32 +11,46 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(16))),
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(16))),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            height: 165,
-            decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16),),),
-            child:ClipRRect(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16),),
+            height: 177,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16), topRight: Radius.circular(16)),
               child: Image.asset(
                 product.imageURL,
-                fit: BoxFit.fill,
-
+                fit: BoxFit.contain,
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
             child: Text(
-              product.name,
-              style: TextStyle(color: Colors.grey),
+              "${product.name}",
+              style: TextStyle(fontWeight: FontWeight.bold),
+              textScaleFactor: 1.15,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text("\$${product.price}", style: TextStyle(fontWeight: FontWeight.bold),),
+            padding: const EdgeInsets.symmetric(horizontal: 6.0),
+            child: Text("\$${product.price}",
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 1.13),
           )
         ],
       ),
