72d758da75ca34b1b6d1dcb77e036a828d0f8ac2
junyangGitHub <yang242@hotmail.com>
2023-10-01 03:11:15 +0800

    Update homepage.dart

--- frontend/lib/homepage.dart
+++ frontend/lib/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/product.dart';
+import 'package:frontend/pages/widgets/vertical_view_listings.dart';
+import 'package:frontend/pages/widgets/taskbar.dart';
 
 void main() {
   runApp(const Home());
@@ -12,7 +15,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HomePage',
-      theme: ThemeData(
+      /*theme: ThemeData(
         colorScheme: const ColorScheme(
           brightness: Brightness.light,
           primary: Color(0xFF5C795B),
@@ -27,9 +30,8 @@ class Home extends StatelessWidget {
           onSurface: Color(0xFF5C795B),
         ),
         useMaterial3: true,
-      ),
+      ),*/
       home: const HomePage(title: 'Home Page'),
-
     );
   }
 }
@@ -52,177 +54,228 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//
 class _HomePageState extends State<HomePage> {
+  final OverlayEntry _overlayEntry = OverlayEntry(
+    builder: (context) => const Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child:  Taskbar(),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(
-                    width: 60 //Width = size of button + sized box in between button and text
-                ),
-                Image.asset('assets/img/ntumart_logo.png', height:50,
-                ),
-                const FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(' NTU Mart ', style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700))
-                ),
-                const SizedBox(
-                    width: 10
-                ),
-                ElevatedButton(
-                  onPressed: (){
-                    //Edit this later to link to chatbot
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Add in chatbot navigation later"),
-                        duration: Duration(milliseconds: 1500),
-                        behavior: SnackBarBehavior.floating,
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 5,
-                    fixedSize: const Size(50, 50),
-                    shape: const CircleBorder(),
-                    backgroundColor: const Color(0xFFFFFFFF),
-                  ),
-                  child: Image.asset('../../assets/images/chatbot_icon.png', fit: BoxFit.fill,
-                  ),
-                ),
-
-              ]
-            ),
-            const SizedBox(
-                height: 15
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SearchBar(
-                leading: const Icon(Icons.search),
-                hintText: 'Search for a product',
-                //hintStyle: const MaterialStatePropertyAll<TextStyle>(TextStyle(fontWeight: FontWeight.w200)),
-                backgroundColor: const MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)
-                ),
-                surfaceTintColor: const MaterialStatePropertyAll<Color>(Color(0xFFF9F9F9)
-                ),
-                shape: MaterialStateProperty.all(const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                )),
-                padding: const MaterialStatePropertyAll<EdgeInsets>(
-                    EdgeInsets.symmetric(horizontal: 16.0)),
-                elevation: null,
-                onTap: (){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text("browse page navigation coming soon!"),
-                      duration: Duration(milliseconds: 1500),
-                      behavior: SnackBarBehavior.floating,
-                    ),
-                  );
-                },
+      body: Stack(
+        children: <Widget>[
+      Overlay(
+            initialEntries: [_overlayEntry],
+          ),   
+          Padding(
+              padding: const EdgeInsets.only(
+                top: 50.0,
+                left: 10.0,
+                right: 10.0,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget> [
-                  const FittedBox(
-                      fit: BoxFit.fitWidth,
-                      child: Text(' Your Categories ', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600))
-                  ),
-                  const SizedBox(
-                      width: 5,
-                  ),
-                  ElevatedButton(
-                    onPressed: (){
-                      //Edit this later to link to homepage
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Sort function will be added l8r'),
-                          duration: Duration(milliseconds: 1500),
-                          behavior: SnackBarBehavior.floating,
-                        ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0), // Adjust the corner radius
-                        ),
-                      ),
-                      surfaceTintColor: const MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)),
-                      backgroundColor: const MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)),
-                      fixedSize: MaterialStateProperty.all( const Size(double.infinity, 30.0)
+              child: SizedBox(
+                height: 750,
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const SizedBox(
+                              width:
+                                  60 //Width = size of button + sized box in between button and text
+                              ),
+                          Image.asset(
+                            'assets/img/ntumart_logo.png',
+                            height: 50,
+                          ),
+                          const FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: Text(' NTU Mart ',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w700))),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              //Edit this later to link to chatbot
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content:
+                                      Text("Add in chatbot navigation later"),
+                                  duration: Duration(milliseconds: 1500),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              fixedSize: const Size(50, 50),
+                              shape: const CircleBorder(),
+                              backgroundColor: const Color(0xFFFFFFFF),
+                            ),
+                            child: Image.asset(
+                              '../../assets/images/chatbot_icon.png',
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ]),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SearchBar(
+                        leading: const Icon(Icons.search),
+                        hintText: 'Search for a product',
+                        //hintStyle: const MaterialStatePropertyAll<TextStyle>(TextStyle(fontWeight: FontWeight.w200)),
+                        backgroundColor: const MaterialStatePropertyAll<Color>(
+                            Color(0xFFFFFFFF)),
+                        surfaceTintColor: const MaterialStatePropertyAll<Color>(
+                            Color(0xFFF9F9F9)),
+                        shape: MaterialStateProperty.all(
+                            const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        )),
+                        padding: const MaterialStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 16.0)),
+                        elevation: null,
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content:
+                                  Text("browse page navigation coming soon!"),
+                              duration: Duration(milliseconds: 1500),
+                              behavior: SnackBarBehavior.floating,
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        const Text('Sort',
-                          style: TextStyle(color: Color(0xFF5D7395), fontWeight: FontWeight.w600, fontSize: 15),
-                        ),
-                        RotatedBox(
-                            quarterTurns: 1,
-                            child: Icon(Icons.compare_arrows_rounded,
-                              color: Colors.grey.shade700 ,),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                      width: 8
-                  ),
-                  ElevatedButton(
-                    onPressed: (){
-                      //Edit this later to link to homepage
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Filter function will be added l8r'),
-                          duration: Duration(milliseconds: 1500),
-                          behavior: SnackBarBehavior.floating,
-                        ),
-                      );
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0), // Adjust the corner radius
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          const FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: Text(' Your Categories ',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600))),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              //Edit this later to link to homepage
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content:
+                                      Text('Sort function will be added l8r'),
+                                  duration: Duration(milliseconds: 1500),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      10.0), // Adjust the corner radius
+                                ),
+                              ),
+                              surfaceTintColor:
+                                  const MaterialStatePropertyAll<Color>(
+                                      Color(0xFFFFFFFF)),
+                              backgroundColor:
+                                  const MaterialStatePropertyAll<Color>(
+                                      Color(0xFFFFFFFF)),
+                              fixedSize: MaterialStateProperty.all(
+                                  const Size(double.infinity, 30.0)),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                const Text(
+                                  'Sort',
+                                  style: TextStyle(
+                                      color: Color(0xFF5D7395),
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 15),
+                                ),
+                                RotatedBox(
+                                  quarterTurns: 1,
+                                  child: Icon(
+                                    Icons.compare_arrows_rounded,
+                                    color: Colors.grey.shade700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 7),
+                          ElevatedButton(
+                            onPressed: () {
+                              //Edit this later to link to homepage
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content:
+                                      Text('Filter function will be added l8r'),
+                                  duration: Duration(milliseconds: 1500),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      10.0), // Adjust the corner radius
+                                ),
+                              ),
+                              surfaceTintColor:
+                                  const MaterialStatePropertyAll<Color>(
+                                      Color(0xFFFFFFFF)),
+                              backgroundColor:
+                                  const MaterialStatePropertyAll<Color>(
+                                      Color(0xFFFFFFFF)),
+                              fixedSize: MaterialStateProperty.all(
+                                const Size(double.infinity, 30.0),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                const Text(
+                                  'Filter',
+                                  style: TextStyle(
+                                      color: Color(0xFF5D7395),
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 15),
+                                ),
+                                Icon(
+                                  Icons.filter_alt_rounded,
+                                  color: Colors.grey.shade700,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      surfaceTintColor: const MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)),
-                      backgroundColor: const MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)),
-                      fixedSize: MaterialStateProperty.all( const Size(double.infinity, 30.0),
-                      ),
                     ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        const Text('Filter',
-                          style: TextStyle(color: Color(0xFF5D7395), fontWeight: FontWeight.w600, fontSize: 15),
-                        ),
-                        Icon(Icons.filter_alt_rounded,
-                            color: Colors.grey.shade700 ,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-                height: 15
-            ),
-          ],
-
-        ),
+                    VerticalViewListings(products: products),
+                  ],
+                ),
+              )),
+               
+        ],
+        
       ),
     );
   }
