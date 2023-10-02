4b57efcbddeb9d774b0ce511f269e6d7b79bef64
dylankwokkkk <dylankwok00@gmail.com>
2023-10-02 21:30:19 +0800

    [feature] filter exact

--- server/src/main/java/com/ntumart/dipapp/api/controllers/ListingController.java
+++ server/src/main/java/com/ntumart/dipapp/api/controllers/ListingController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.ntumart.dipapp.models.Product;
+import java.util.ArrayList;
+
+import com.ntumart.dipapp.api.repository.ListingRepository;
 import com.ntumart.dipapp.api.service.ListingService;
 import com.ntumart.dipapp.models.Product;
 
@@ -15,12 +19,21 @@ import com.ntumart.dipapp.models.Product;
 @RequestMapping("/api/v1")
 public class ListingController {
     @Autowired
+
     ListingService listingService;
     @GetMapping("/product/listing")
     public ResponseEntity<List<Product>> listFilteredAndSortedProducts(
             @RequestParam(required = false) String sortBy)  {
 
         List<Product> products = listingService.getFilteredAndSortedProducts(sortBy);
+
+    @Autowired
+
+    ListingRepository listingRepository;
+
+     @GetMapping("/product/listing")
+    public ResponseEntity<List<Object>> listAllProducts(){
+        List<Object> products = listingService.getNameANDDescriptionANDProductLikes();
         return ResponseEntity.ok(products);
     }
 }
