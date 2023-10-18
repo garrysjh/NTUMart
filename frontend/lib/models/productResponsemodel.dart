31465166c9d98c2f39eac2c12a9cccacbaabed53
junyangGitHub <yang242@hotmail.com>
2023-10-18 15:36:10 +0800

    Update productResponsemodel.dart

--- frontend/lib/models/productResponsemodel.dart
+++ frontend/lib/models/productResponsemodel.dart
@@ -21,6 +21,7 @@ class ProductResponse {
     List<String?>? binaryImageDataList,
   }) : binaryImageDataList = binaryImageDataList ?? [productPic, productPic2, productPic3, productPic4];
 
+  // Getters and Setters
   // Getters
   String? get getProductName => productName;
   String? get getDescription => description;
@@ -43,21 +44,69 @@ class ProductResponse {
   set setProductPic4(String? value) => productPic4 = value;
   set setBinaryImageDataList(List<String?>? value) => binaryImageDataList = value ?? [productPic, productPic2, productPic3, productPic4];
 
-// Additional constructor to create an instance from a map
+  // Additional constructor to create an instance from a map
   ProductResponse.fromMap(Map<String, dynamic> productData)
-      : productName = productData["productName"] ?? '',
-        description = productData["description"] ?? '',
-        price = productData["price"]?.toDouble() ?? -1.0,
-        quantity = productData["quantity"]?.toInt() ?? -1,
-        productPic = productData["productImages"][0]["content"] ?? '',
-        productPic2 = productData["productImages"][1]["content"] ?? '',
-        productPic3 = productData["productImages"][2]["content"] ?? '',
-        productPic4 = productData["productImages"][3]["content"] ?? '',
-        binaryImageDataList = [
+    : productName = productData["productName"] ?? '',
+      description = productData["description"] ?? '',
+      price = productData["price"]?.toDouble() ?? -1.0,
+      quantity = productData["quantity"]?.toInt() ?? -1,
+      productPic = (productData["productImages"].length > 0)
+          ? productData["productImages"][0]["content"] ?? ''
+          : '',
+      productPic2 = (productData["productImages"].length > 1)
+          ? productData["productImages"][1]["content"] ?? ''
+          : '',
+      productPic3 = (productData["productImages"].length > 2)
+          ? productData["productImages"][2]["content"] ?? ''
+          : '',
+      productPic4 = (productData["productImages"].length > 3)
+          ? productData["productImages"][3]["content"] ?? ''
+          : '',
+      binaryImageDataList = [
+        if (productData["productImages"].length > 0)
           productData["productImages"][0]["content"] ?? '',
+        if (productData["productImages"].length > 1)
           productData["productImages"][1]["content"] ?? '',
+        if (productData["productImages"].length > 2)
           productData["productImages"][2]["content"] ?? '',
+        if (productData["productImages"].length > 3)
           productData["productImages"][3]["content"] ?? '',
-        ];
+      ];
 
+  // Factory method to create an instance from JSON
+  factory ProductResponse.fromJson(Map<String, dynamic> json) {
+    return ProductResponse(
+      productName: json['productName'],
+      description: json['description'],
+      price: json['price']?.toDouble(),
+      quantity: json['quantity']?.toInt(),
+      productPic: json['productPic'],
+      productPic2: json['productPic2'],
+      productPic3: json['productPic3'],
+      productPic4: json['productPic4'],
+      binaryImageDataList: List<String?>.from(json['binaryImageDataList']),
+    );
+  }
+
+  // Convert the instance to a JSON representation
+  Map<String, dynamic> toJson() {
+    return {
+      'productName': productName,
+      'description': description,
+      'price': price,
+      'quantity': quantity,
+      'productPic': productPic,
+      'productPic2': productPic2,
+      'productPic3': productPic3,
+      'productPic4': productPic4,
+      'binaryImageDataList': binaryImageDataList,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'ProductResponse{productName: $productName, description: $description, price: $price, quantity: $quantity, '
+        'productPic: $productPic, productPic2: $productPic2, productPic3: $productPic3, productPic4: $productPic4, '
+        'binaryImageDataList: $binaryImageDataList}';
+  }
 }
