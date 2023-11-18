13a241f29ee77681b980ac032e8434e2a783fd5e
arielgohsn <arielgohsn@gmail.com>
2023-11-19 00:35:18 +0800

    base onboarding screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/homepage.dart';
+import 'package:frontend/onboarding.dart';
 import 'package:frontend/pages/login_page.dart';
 import 'package:flutter/services.dart';
 import 'package:frontend/pages/jwtTokenDecryptService.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 var URL = 'http://10.0.2.2:8080/api/v1'; //replace this with ur local ip / lan ip for devices connecting on same lan / server ip if hosted
+int? initScreen;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(<DeviceOrientation>[
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = (await prefs.getInt("initScreen"))!;
+  await prefs.setInt("initScreen", 1);
+  print('initScreen ${initScreen}');
   if (await JwtTokenDecryptService.hasValidToken()) {
     runApp(const Home());
   }
@@ -23,10 +30,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'NTUMart',
-      home: LoginPage(),
+      initialRoute: initScreen == 0 || initScreen == null ? "first" : "/",
+      routes: {
+        '/': (context) => LoginPage(),
+        "first": (context) => LoginPage(), //change to onboarding screen
+      },
     );
   }
 }
---
+++ frontend/lib/onboarding.dart
@@ -0,0 +1,320 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/login_page.dart';
+import 'package:frontend/main.dart';
+import 'package:onboarding/onboarding.dart';
+
+void main() {
+  runApp(const OnBoardingScreen());
+}
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onboarding Screen',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xFF5C795B),
+          onPrimary: Color(0xFFEAEAEA),
+          secondary: Color(0xFF5D7395),
+          onSecondary: Color(0xFFEAEAEA),
+          error: Color(0xFFF32424),
+          onError: Color(0xFFF32424),
+          background: Color(0xFFF9F9F9),
+          onBackground: Color(0xFFFFFFFF),
+          surface: Color(0xFFEAEAEA),
+          onSurface: Color(0xFF5C795B),
+        ),
+      ),
+      home: const OnBoardingScreenPage(title: 'Category Selection Page'),
+    );
+  }
+}
+
+class OnBoardingScreenPage extends StatefulWidget {
+  const OnBoardingScreenPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<OnBoardingScreenPage> createState() => OnBoardingScreenPageState();
+}
+
+class OnBoardingScreenPageState extends State<OnBoardingScreenPage> {
+  late Material materialButton;
+  late int index;
+  final onboardingPagesList = [
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: background,
+          border: Border.all(
+            width: 0.0,
+            color: background,
+          ),
+        ),
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 45.0,
+                  vertical: 90.0,
+                ),
+                child: Image.asset('assets/images/facebook.png',
+                    color: pageImageColor),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'SECURED BACKUP',
+                    style: pageTitleStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Keep your files in closed safe so you can\'t lose them. Consider TrueNAS.',
+                    style: pageInfoStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: background,
+          border: Border.all(
+            width: 0.0,
+            color: background,
+          ),
+        ),
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 45.0,
+                  vertical: 90.0,
+                ),
+                child: Image.asset('assets/images/twitter.png',
+                    color: pageImageColor),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'CHANGE AND RISE',
+                    style: pageTitleStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Give others access to any file or folders you choose',
+                    style: pageInfoStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: background,
+          border: Border.all(
+            width: 0.0,
+            color: background,
+          ),
+        ),
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 45.0,
+                  vertical: 90.0,
+                ),
+                child: Image.asset('assets/images/instagram.png',
+                    color: pageImageColor),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'EASY ACCESS',
+                    style: pageTitleStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Reach your files anytime from any devices anywhere',
+                    style: pageInfoStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    materialButton = _skipButton();
+    index = 0;
+  }
+
+  Material _skipButton({void Function(int)? setIndex}) {
+    return Material(
+      borderRadius: defaultSkipButtonBorderRadius,
+      color: defaultSkipButtonColor,
+      child: InkWell(
+        borderRadius: defaultSkipButtonBorderRadius,
+        onTap: () {
+          if (setIndex != null) {
+            index = index + 1;
+            setIndex(index);
+          }
+        },
+        child: const Padding(
+          padding: defaultSkipButtonPadding,
+          child: Text(
+            'Next',
+            style: defaultSkipButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material get _signupButton {
+    return Material(
+      borderRadius: defaultProceedButtonBorderRadius,
+      color: const Color(0xFF5C795B),
+      child: InkWell(
+        borderRadius: defaultProceedButtonBorderRadius,
+        onTap: () {},
+        child: const Padding(
+          padding: defaultProceedButtonPadding,
+          child: Text(
+            'Get Started',
+            style: defaultProceedButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Onboarding Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Onboarding(
+          pages: onboardingPagesList,
+          onPageChange: (int pageIndex) {
+            index = pageIndex;
+          },
+          startPageIndex: 0,
+          footerBuilder: (context, dragDistance, pagesLength, setIndex) {
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                color: background,
+                border: Border.all(
+                  width: 0.0,
+                  color: background,
+                ),
+              ),
+              child: ColoredBox(
+                color: background,
+                child: Padding(
+                  padding: const EdgeInsets.all(45.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomIndicator(
+                        netDragPercent: dragDistance,
+                        pagesLength: pagesLength,
+                          indicator: Indicator(
+                            indicatorDesign: IndicatorDesign.polygon(
+                              polygonDesign: PolygonDesign(
+                                polygon: DesignType.polygon_circle,
+                              ),
+                            ),
+                          ),
+                      ),
+                      index == pagesLength - 1
+                          ? _signupButton
+                          : _skipButton(setIndex: setIndex)
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+}
+
+
